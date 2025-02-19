ec0bc25621092bb3ecb708eeca471275684a0a3e
Goutham Ganapathiappan <gganapathiappan1@student.gsu.edu>
2025-02-19 01:21:02 -0500

    Added Vercel Analytics to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
-
+import { Analytics } from "@vercel/analytics/react"; 
 
 const App = () => {
   return (
@@ -20,6 +20,7 @@ const App = () => {
         <Education />
         <Contact />
       </div>
+      <Analytics /> 
     </main>
   );
 };
